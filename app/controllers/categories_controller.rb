04bc8d429809ad5cd043f87f255810dd75ee11eb
ObaTakeshi <s153025@matsu.shimane-u.ac.jp>
2017-11-02 11:58:08 +0900

    カテゴリー詳細ページの作成

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,6 +10,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
+    @edits = Edit.where(category_id: params[:id])
   end
 
   # GET /categories/new
