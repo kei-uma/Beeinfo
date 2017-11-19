ae15f8b7592063220251ce5d44dd92ad705fc9dc
ObaTakeshi <s153025@matsu.shimane-u.ac.jp>
2017-11-19 17:33:28 +0900

    カテゴリー新規追加時にnameが空でも追加できる問題を修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
   validates :name, uniqueness: true
+  validates_presence_of :name
   has_many :edits
   has_many :user_categories
   has_many :users, through: :user_categories
