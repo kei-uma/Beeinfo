c5816f92d0dc21cdecc53e2cc627da4275a114e7
kei-uma <s153021@matsu.shimane-u.ac.jp>
2017-11-23 02:13:08 +0900

    記事作成画面からtrendsに戻った際に前のツイートが残っているのを修正

--- app/controllers/trends_controller.rb
+++ app/controllers/trends_controller.rb
@@ -1,9 +1,10 @@
 class TrendsController < ApplicationController
   before_action :set_trend, only: [:show, :edit, :update, :destroy]
-
+$twiGetId = Array.new
   # GET /trends
   # GET /trends.json
   def index
+    $twiGetId = Array.new
     @trends = Trend.all
   end
 
@@ -11,11 +12,13 @@ class TrendsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_trend
+      $twiGetId = Array.new
       @trend = Trend.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def trend_params
+      $twiGetId = Array.new
       params.fetch(:trend, {})
     end
 end
