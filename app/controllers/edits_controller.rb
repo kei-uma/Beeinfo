1e2a9e779c07670e380d5f7c2b08a4493196735d
kei-uma <s153021@matsu.shimane-u.ac.jp>
2017-12-02 16:52:58 +0900

    addにデッバッグを追加

--- app/controllers/edits_controller.rb
+++ app/controllers/edits_controller.rb
@@ -49,7 +49,7 @@ $t = 0
   def create
     @edit = Edit.new(edit_params)
     @edit.twitter_datum_ids = $twiGetId
-    logger.debug("Log2 : " +@edit.twitter_datum_ids)
+    logger.debug($twiGetId)
     @articles = TwitterDatum.all.order(created_at: 'desc')
     respond_to do |format|
       if @edit.save!
@@ -66,7 +66,7 @@ $t = 0
   def add
     $twiGetId << params[:id]
     $twiGetId.uniq!
-    logger.debug("Log1 : " +$twiGetId)
+    logger.debug($twiGetId)
     end
 
   # PATCH/PUT /edits/1
